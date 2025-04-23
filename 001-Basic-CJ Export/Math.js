b1982c90d37f92cc1ca96e25f516ccd66a1804c4
Lokman Sarkar <lokmansarker13@gmail.com>
2025-04-23 19:35:30 +0600

    Basics of node js added

---
+++ 001-Basic-CJ Export/Math.js
@@ -0,0 +1,9 @@
+ const PI = 3.1416
+ const circlArea = (r) => {
+    return PI * Math.sqrt(r)
+ }
+
+ module.exports = {PI, circlArea}
+
+
+
---
+++ 001-Basic-CJ Export/app.js
@@ -0,0 +1,10 @@
+// console.log(global.crypto)
+
+
+const mathFunctons = require('./Math')
+console.log(mathFunctons)
+
+console.log("Area of Circle ",  mathFunctons.circlArea(5).toFixed(2))
+
+
+

a8873142db21e840e69b43666a6ba5c10646b16c
Lokman Sarkar <lokmansarker13@gmail.com>
2025-04-23 19:36:05 +0600

    Node JS Named Export Added

---
+++ 002-Named-Export/app.js
@@ -0,0 +1,6 @@
+const {add, subt, mult , div} = require('./mathFunctions')
+
+console.log(add(4,5))
+console.log(subt(4,5))
+console.log(mult(4,5))
+console.log(div(4,5))
---
+++ 002-Named-Export/mathFunctions.js
@@ -0,0 +1,18 @@
+const add = (a, b) => {
+    return a + b 
+}
+
+
+const subt= (a, b) => {
+    return a - b 
+}
+
+const mult= (a, b) => {
+    return a * b 
+}
+
+const div= (a, b) => {
+    return a / b 
+}
+
+module.exports = {add, subt, mult, div}

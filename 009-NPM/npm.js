74fcc54418bd509752e221e9fff66e70c94ff75c
Lokman Sarkar <lokmansarker13@gmail.com>
2025-04-27 19:37:48 +0600

    Gitignore added

---
+++ 009-NPM/npm.js
@@ -0,0 +1,8 @@
+// npm
+import chalk from "chalk";
+
+
+chalk.level = 3; // 0 = no colors, 1 = basic colors, 2 = medium colors, 3 = true color (16 million colors)
+console.log(chalk.red("Hello") + " World" + chalk.blue("!")); // Hello World!
+console.log(chalk.red("Hello") + " World" + chalk.blue("!")); // Hello World!
+console.log(chalk.red("Hello") + " World" + chalk.magenta("!")); // Hello World!

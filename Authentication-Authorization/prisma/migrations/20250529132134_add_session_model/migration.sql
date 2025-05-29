55ec32c6b28572193fc467de22eee4d916234bfd
Lokman Sarkar <lokmansarker13@gmail.com>
2025-05-29 19:24:58 +0600

    Session Schema added for hybrid authentication

---
+++ Authentication-Authorization/prisma/migrations/20250529132134_add_session_model/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `valid` BOOLEAN NOT NULL DEFAULT true,
+    `userAgent` VARCHAR(255) NULL,
+    `ipAddress` VARCHAR(255) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Session` ADD CONSTRAINT `Session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
